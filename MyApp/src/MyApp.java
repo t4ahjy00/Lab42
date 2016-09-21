d70057e5a83a528f9b9238694a334c7991c928c0
Jyrki <t4ahjy00@students.oamk.fi>
2016-09-21 15:11:54 +0300

    Added method changePrice

--- MyApp/src/MyApp.java
+++ MyApp/src/MyApp.java
@@ -3,6 +3,7 @@ public class MyApp {
     // Author: Jyrki
     public static void main(String[] args) {
        Product product = new Product("Edam", 3.3, 120);
+       product.changePrice(2.2);
        System.out.println("Product value is " + product.countValue());
        product.printProduct();
     } 
@@ -22,4 +23,11 @@ return(amount * price);
 public void printProduct() {
 System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
 }
+
+public double changePrice (double newPrice)
+{
+    price = newPrice;
+    return price;
+}
+
 }
