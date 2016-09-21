b28f564d2fff6bb4f114b5c5687dbe6e5eaa3ea9
Jyrki <t4ahjy00@students.oamk.fi>
2016-09-21 14:57:31 +0300

    Added new method changeBalance

--- MyApp/src/MyApp.java
+++ MyApp/src/MyApp.java
@@ -3,6 +3,7 @@ public class MyApp {
     // Author: Jyrki
     public static void main(String[] args) {
        Product product = new Product("Edam", 3.3, 120);
+       product.changeBalance(100);
        System.out.println("Product value is " + product.countValue());
        product.printProduct();
     } 
@@ -16,10 +17,18 @@ this.name = name;
 this.price = price;
 this.amount = amount;
 }
+
 public double countValue() {
 return(amount * price);
 }
+
 public void printProduct() {
 System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
+}
+
+public int changeBalance (int newValue)
+{
+    amount = newValue;
+    return amount;
 }
 }
